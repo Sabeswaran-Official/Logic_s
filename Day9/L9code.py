6263378d95f29d794a7db53db245519ab9aa795b
SABESWARAN <sabeswaran2002@gmail.com>
2025-09-07 16:08:42 +0530

    some updates

--- Day9/L9code.py
+++ Day9/L9code.py
@@ -41,3 +41,33 @@ print(solution([2,2,2,4,4,6,2,6,8]))
 
 # 54. Spiral matrix
 
+class spiralMatrix:
+    def solution(matrix):
+        m=len(matrix)
+        n=len(matrix[0])
+        top,bottom=0,m-1
+        left,right=0,n-1
+        result=[]
+
+        while top<=bottom and left<=right:
+            for i in range(left,right+1):
+                result.append(matrix[top][i])
+            top+=1
+
+            for i in range(top,bottom+1):
+                result.append(matrix[i][right])
+            right-=1
+
+            if top<=bottom:
+                for i in range(right,left-1,-1):
+                    result.append(matrix[bottom][i])
+                bottom-=1
+            if left<=right:
+                for i in range(bottom,top-1,-1):
+                    result.append(matrix[i][left])
+                left+=1
+
+            return result
+sm=spiralMatrix
+print(sm.solution([[1,2,3],[4,5,6],[7,8,9]]))
+
