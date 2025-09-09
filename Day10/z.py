acac2f20d32846dc9bf42b8c9046144af160c899
SABESWARAN <sabeswaran2002@gmail.com>
2025-09-09 18:14:25 +0530

    some updates

--- Day10/z.py
+++ Day10/z.py
@@ -1,5 +1,5 @@
 # Segregate even & odd n a alternative position  input : [1,2,4,3,5,7,8,6]  ouput : [1,8,3,6,5,4,7,2]  -don't use external array.Do it in-place
-
+'''
 class EvenOdd:
     def solution(arr):
         arr.sort(key=lambda x:(x%2==0 ,-x if x%2==0 else x))
@@ -23,3 +23,69 @@ class EvenOdd:
         return f"{arr} == {result}"
 sol=EvenOdd
 print(sol.solution([1,2,4,3,5,7,8,6,9]))
+
+
+# 165.Compare version numbers
+class VersNo:
+    def solution(version1,version2):
+        ver1=list(map(int,version1.split(".")))
+        ver2=list(map(int,version2.split(".")))
+        max_length=max(len(ver1),len(ver2))
+        ver1.extend([0]*(max_length-len(ver1)))
+        ver2.extend([0]* (max_length-len(ver2)))
+        
+        for i in range(max_length):
+            if ver1[i]<ver2[i]:
+                return -1
+            elif ver1[i]>ver2[i]:
+                return 1
+        return 0
+
+vn=VersNo
+print(vn.solution("1.0","1.0.0.0"))
+
+
+# 44.Wildcard matching
+def wildCardMatching(s,p):
+    i,j=0,0
+    start_ind=-1
+    match=0
+
+    while i<len(s):
+        if j<len(p) and (p[j]==s[i] or p[j]=="?"):
+            i+=1;j+=1
+        elif j<len(p) and p[j]=="*":
+            start_ind=j
+            match=i
+            j+=1
+        elif start_ind!=-1:
+            j=start_ind+1
+            match+=1
+            i=match
+        else:
+            return False
+
+    while j<len(p) and p[j]=="*":
+        j+=1
+
+    return j==len(p)
+print(wildCardMatching( "cb","?a"))
+'''
+
+# Arange wave no's I/P   - [20, 40, 30, 50, 70, 80]  , O/P - [20, 40, 30, 70, 50, 80]    . Explanation :  a1 <= a2 >= a3 <= a4 >= a5....
+def solution(arr):
+    i=0
+    j=1
+    n=len(arr)
+
+    while j<n:
+        if i%2==0:
+            if arr[i]>arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+                
+        else:
+            if arr[i]<arr[j]:
+                arr[i],arr[j]=arr[j],arr[i]
+        i+=1;j+=1
+    return arr
+print(solution([20, 40, 30, 50, 70, 80]))
